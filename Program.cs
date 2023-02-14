8923fd8316aa9a8aca0bccea1cd3f255ab522165
akashbokde98 <akashbokde98@gmail.com>
2023-02-14 23:39:38 +0530

    Added: Check User Phone Number

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace UserRegistration
         public static string checkFirstName = "^[A-Z][a-z]{3,}$";
         public static string checkLastName = "^[A-Z][a-z]{3,}$";
         public static string checkEmailId = "^[a-z{3,}]+[@+-.0-9a-z{3,}]+$";
+        public static string checkphoneNumber = "^91[ ][0-9]+$";
     }
     internal class Program
     {
@@ -24,6 +25,10 @@ namespace UserRegistration
             Regex rg3 = new Regex(Pattern.checkEmailId);
             Console.WriteLine("Enter User E-mail Id-");
             Console.WriteLine(rg3.IsMatch(Console.ReadLine()));
+
+            Regex rg4 = new Regex(Pattern.checkphoneNumber);
+            Console.WriteLine("Enter User Phone Number-");
+            Console.WriteLine(rg4.IsMatch(Console.ReadLine()));
 
         }
     }
