35bf8d7c1807e6882dc92d15edec5894ff178a32
akashbokde98 <akashbokde98@gmail.com>
2023-02-15 00:19:51 +0530

    Added: Check User Password_1

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace UserRegistration
         public static string checkLastName = "^[A-Z][a-z]{3,}$";
         public static string checkEmailId = "^[a-z{3,}]+[@+-.0-9a-z{3,}]+$";
         public static string checkphoneNumber = "^91[ ][0-9]+$";
+        public static string checkPassword_1 = "^[A-Za-z]{8,}$";
     }
     internal class Program
     {
@@ -29,6 +30,10 @@ namespace UserRegistration
             Regex rg4 = new Regex(Pattern.checkphoneNumber);
             Console.WriteLine("Enter User Phone Number-");
             Console.WriteLine(rg4.IsMatch(Console.ReadLine()));
+
+            Regex rg5 = new Regex(Pattern.checkPassword_1);
+            Console.WriteLine("Enter User Password With Min 8 Character-");
+            Console.WriteLine(rg5.IsMatch(Console.ReadLine()));
 
         }
     }
