f2719e3cc340d2d77b861ac855a4713a5b907e71
akashbokde98 <akashbokde98@gmail.com>
2023-02-14 21:06:18 +0530

    Added: Check User E-mail

--- Program.cs
+++ Program.cs
@@ -5,21 +5,26 @@ namespace UserRegistration
 {
     public static class Pattern
     {
-        public static string CheckFirstName = "^[A-Z][a-z]{3,}$";
-        public static string CheckLastName = "^[A-Z][a-z]{3,}$";
+        public static string checkFirstName = "^[A-Z][a-z]{3,}$";
+        public static string checkLastName = "^[A-Z][a-z]{3,}$";
+        public static string checkEmailId = "^[a-z{3,}]+[@+-.0-9a-z{3,}]+$";
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Regex rg = new Regex(Pattern.CheckFirstName);
-
+            Regex rg1 = new Regex(Pattern.checkFirstName);
             Console.WriteLine("Enter User First Name-");
-            Console.WriteLine(rg.IsMatch(Console.ReadLine()));
+            Console.WriteLine(rg1.IsMatch(Console.ReadLine()));
 
+            Regex rg2 = new Regex(Pattern.checkLastName);
             Console.WriteLine("Enter User Last Name-");
-            Console.WriteLine(rg.IsMatch(Console.ReadLine()));
-            
+            Console.WriteLine(rg2.IsMatch(Console.ReadLine()));
+
+            Regex rg3 = new Regex(Pattern.checkEmailId);
+            Console.WriteLine("Enter User E-mail Id-");
+            Console.WriteLine(rg3.IsMatch(Console.ReadLine()));
+
         }
     }
 }
