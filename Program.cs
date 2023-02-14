634e700d91fc125886271cc8d9f1b378e6b10456
akashbokde98 <akashbokde98@gmail.com>
2023-02-14 16:00:53 +0530

    Added: Checking Last Name

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ namespace UserRegistration
     public static class Pattern
     {
         public static string CheckFirstName = "^[A-Z][a-z]{3,}$";
+        public static string CheckLastName = "^[A-Z][a-z]{3,}$";
     }
     internal class Program
     {
         static void Main(string[] args)
         {
             Regex rg = new Regex(Pattern.CheckFirstName);
+
+            Console.WriteLine("Enter User First Name-");
+            Console.WriteLine(rg.IsMatch(Console.ReadLine()));
 
             Console.WriteLine("Enter User Last Name-");
             Console.WriteLine(rg.IsMatch(Console.ReadLine()));
